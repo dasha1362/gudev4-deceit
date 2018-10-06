7b70173fb554e8edefb177b1a9dbaebcfb51df9a
Dasha <darya_shum@live.co.uk>
2018-10-06 10:40:58 +0100

    level 1 both players can move and jump

---
+++ Parkour Piggies/Assets/Scripts/Player1Controller.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Player1Controller : MonoBehaviour
+{
+
+    public float walkSpeed;
+    public float jumpSpeed;
+    public float gravityScale;
+    //flag to keep track of whether a jump started
+    bool pressedJump = false;
+
+    CharacterController cc;
+    private Vector3 moveDirection;
+
+    Rigidbody rb;
+    Collider coll;
+
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        WalkHandler();
+
+        JumpHandler();
+
+        cc.Move(moveDirection * Time.deltaTime);
+    }
+
+    void WalkHandler()
+    {
+        moveDirection = new Vector3(Input.GetAxis("Horizontal") * walkSpeed, moveDirection.y, Input.GetAxis("Vertical") * walkSpeed);
+    }
+
+    void JumpHandler()
+    {
+        if (cc.isGrounded && Input.GetButtonDown("Jump"))
+        {
+            moveDirection.y = jumpSpeed;
+        }
+
+        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale);
+    }
+
+}
---
+++ Parkour Piggies/Assets/Scripts/Player2Controller.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Player2Controller : MonoBehaviour
+{
+
+    public float walkSpeed;
+    public float jumpSpeed;
+    public float gravityScale;
+    //flag to keep track of whether a jump started
+    bool pressedJump = false;
+
+    CharacterController cc;
+    private Vector3 moveDirection;
+
+    Rigidbody rb;
+    Collider coll;
+
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        WalkHandler();
+
+        JumpHandler();
+
+        cc.Move(moveDirection * Time.deltaTime);
+    }
+
+    void WalkHandler()
+    {
+        moveDirection = new Vector3(Input.GetAxis("P2_Horizontal") * walkSpeed, moveDirection.y, Input.GetAxis("P2_Vertical") * walkSpeed);
+    }
+
+    void JumpHandler()
+    {
+        if (cc.isGrounded && Input.GetButtonDown("P2_Jump"))
+        {
+            moveDirection.y = jumpSpeed;
+        }
+
+        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale);
+    }
+}
