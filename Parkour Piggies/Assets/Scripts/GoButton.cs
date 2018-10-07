8baf38da52d3e5dc0e6d966962c92fe4e79e175b
Dasha <darya_shum@live.co.uk>
2018-10-07 14:01:30 +0100

    added main menu with button

---
+++ Parkour Piggies/Assets/Scripts/GoButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoButton : MonoBehaviour {
+
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
